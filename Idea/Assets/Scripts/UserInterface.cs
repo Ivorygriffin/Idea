3b59ae859b66353fe1c9d01bb6943bffdb018ad1
Ivorygriffin <s210559@students.aie.edu.au>
2021-12-11 14:06:00 +1030

    initial

---
+++ Idea/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    //Variables
+    public Slider convinced;
+    public Slider suspicion;
+
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void CheckConvinced()
+    {
+
+
+
+
+    }
+    public void CheckSuspicion()
+    {
+
+
+
+
+    }
+    public void NewCharacter()
+    {
+
+
+    }
+    public void TimerComplete()
+    {
+
+    }
+
+
+
+
+
+
+
+}
